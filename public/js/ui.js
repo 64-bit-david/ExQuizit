3b51451e510af1c4953016b2d4828f283227b17c
David Williamson <dwilliamsonwebdev@gmail.com>
2021-02-03 12:08:08 +0000

    mobile-nav update

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,29 +1,52 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const nestedDropBtns = document.querySelectorAll('.dropdown-toggle');
+  const nestedDropMenus = document.querySelectorAll('.dropdown-menu-mobile');
+  let isFirstDropDownActive = false;
+  let isSecondDropDownActive = false;
 
   burger.addEventListener('click', () => {
-    //toggle nav
-    nav.classList.toggle('nav-active');
+    //toggle nested drop-downs
+    if (isFirstDropDownActive && isSecondDropDownActive) {
+      nestedDropMenus.forEach(dropMenu => {
+        dropMenu.classList.remove('active');
+      })
+      isSecondDropDownActive = !isSecondDropDownActive;
+      return;
 
-    //animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .2}s`
+    }
+    //toggle nav at start
+    if (!isFirstDropDownActive && !isSecondDropDownActive) {
+      nav.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
+      isFirstDropDownActive = !isFirstDropDownActive;
+      return;
+
+
+    } else if (isFirstDropDownActive && !isSecondDropDownActive) {
+      burger.classList.toggle('toggle');
+      nav.classList.toggle('nav-active');
+      isFirstDropDownActive = !isFirstDropDownActive;
+      return;
+    }
+  });
+
+  //toggle clicked sub dropdown
+  nestedDropBtns.forEach((dropBtn, index) => {
+    dropBtn.addEventListener('click', () => {
+      if (isFirstDropDownActive) {
+        isSecondDropDownActive = !isSecondDropDownActive;
+        nestedDropMenus[index].classList.toggle('active')
       }
-    })
-    //burger animation
-    burger.classList.toggle('toggle');
+    });
   });
 }
+
 
 const dropDownToggle = () => {
   const dropDownBtns = document.querySelectorAll('.dropdown-toggle');
   const dropDownMenus = document.querySelectorAll('.dropdown-menu');
-
   dropDownBtns.forEach((dropDownBtn, index) => {
     dropDownBtn.addEventListener("click", () => {
       dropDownMenus[index].classList.toggle('active');
